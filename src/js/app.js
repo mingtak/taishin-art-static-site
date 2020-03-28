9612a53b5d77e6b5ba6667d984b0705eab58a506
karun <sthkarun@gmail.com>
2020-03-28 14:31:51 +0545

    notion bug fixed

--- src/js/app.js
+++ src/js/app.js
@@ -12,6 +12,53 @@ var AOS = require('aos');
 
 jQuery.bridget( 'isotope', Isotope );
 
+
+
+jQuery(function() {
+	initNavigationSelect();
+	switchTabBySelect();
+	initMobileNav();
+	initFixedHeader();
+	initSameHeight();
+	initFilter();
+	initSlickSlider();
+	initAos();
+	initOpenClose();
+	initSmoothScroll();
+});
+
+
+// open-close init
+function initOpenClose() {
+	jQuery('.share-block').openClose({
+		activeClass: 'active',
+		opener: '.opener',
+		slider: '.share-holder',
+		animSpeed: 400,
+		effect: 'slide'
+	});
+
+	jQuery('.share-content').openClose({
+		activeClass: 'active',
+		opener: '.opener',
+		slider: '.share-holder',
+		animSpeed: 400,
+		effect: 'fade'
+	});
+
+	jQuery('.shareit').openClose({
+		activeClass: 'active',
+		opener: '.opener',
+		slider: '.drop',
+		animSpeed: 400,
+		effect: 'fade'
+	});
+}
+
+
+
+
+
 // loaded state script
 (function(w) {
 	w.addEventListener('load', function() {
@@ -24,18 +71,6 @@ jQuery.bridget( 'isotope', Isotope );
 
 
 
-jQuery(function() {
-	initNavigationSelect();
-	switchTabBySelect();
-	initMobileNav();
-	initFixedHeader();
-	initSameHeight();
-	initFilter();
-	initSlickSlider();
-	initAos();
-	initSmoothScroll();
-});
-
 function initSmoothScroll() {
 	var btn = jQuery('.btn-scroll');
 
@@ -44,6 +79,20 @@ function initSmoothScroll() {
 		jQuery('body, html').animate({
 			scrollTop: jQuery(btn.attr('href')).offset().top
 		}, 500);
+	});
+
+	
+
+	var scroll = jQuery('.scroll');
+	scroll.on('click', function(e) {
+		e.preventDefault();
+		var target = $(this).attr('href'), $target = $(target); {
+			$('html, body').stop().animate({
+				'scrollTop' : $target.offset().top - jQuery('#header').outerHeight()
+			}, 900, 'swing', function() {
+				window.location.hash = target;
+			});
+		}
 	});
 }
 
@@ -87,7 +136,7 @@ function initFilter() {
 		});
 	});
 
-	if (location.hash.length && location.hash.replace(/_/g, ' ').slice(1) != 'all') {
+	if (location.hash.length && location.hash.replace(/_/g, ' ').slice(1) != 'all' && location.hash.replace(/_/g, ' ').slice(1) != 'section1') {
 		
 		$container.isotope({
 			filter: '[data-filter*=' + location.hash.replace(/_/g, ' ').slice(1) + ']'
@@ -168,6 +217,7 @@ function initNavigationSelect() {
 
 // mobile menu init
 function initMobileNav() {
+
 	jQuery('body').mobileNav({
 		menuActiveClass: 'nav-active',
 		menuOpener: '.nav-opener',
@@ -509,4 +559,199 @@ function initMobileNav() {
 		boxes.filter(':last').addClass(options.rightEdgeClass);
 		return calcHeight;
 	}
+}(jQuery));
+
+
+/*
+ * jQuery Open/Close plugin
+ */
+ ;(function($) {
+ 	function OpenClose(options) {
+ 		this.options = $.extend({
+ 			addClassBeforeAnimation: true,
+ 			hideOnClickOutside: false,
+ 			activeClass: 'active',
+ 			opener: '.opener',
+ 			slider: '.slide',
+ 			animSpeed: 400,
+ 			effect: 'fade',
+ 			event: 'click'
+ 		}, options);
+ 		this.init();
+ 	}
+ 	OpenClose.prototype = {
+ 		init: function() {
+ 			if (this.options.holder) {
+ 				this.findElements();
+ 				this.attachEvents();
+ 				this.makeCallback('onInit', this);
+ 			}
+ 		},
+ 		findElements: function() {
+ 			this.holder = $(this.options.holder);
+ 			this.opener = this.holder.find(this.options.opener);
+ 			this.slider = this.holder.find(this.options.slider);
+ 		},
+ 		attachEvents: function() {
+			// add handler
+			var self = this;
+			this.eventHandler = function(e) {
+				e.preventDefault();
+				if (self.slider.hasClass(slideHiddenClass)) {
+					self.showSlide();
+				} else {
+					self.hideSlide();
+				}
+			};
+			self.opener.on(self.options.event, this.eventHandler);
+
+			// hover mode handler
+			if (self.options.event === 'hover') {
+				self.opener.on('mouseenter', function() {
+					if (!self.holder.hasClass(self.options.activeClass)) {
+						self.showSlide();
+					}
+				});
+				self.holder.on('mouseleave', function() {
+					self.hideSlide();
+				});
+			}
+
+			// outside click handler
+			self.outsideClickHandler = function(e) {
+				if (self.options.hideOnClickOutside) {
+					var target = $(e.target);
+					if (!target.is(self.holder) && !target.closest(self.holder).length) {
+						self.hideSlide();
+					}
+				}
+			};
+
+			// set initial styles
+			if (this.holder.hasClass(this.options.activeClass)) {
+				$(document).on('click touchstart', self.outsideClickHandler);
+			} else {
+				this.slider.addClass(slideHiddenClass);
+			}
+		},
+		showSlide: function() {
+			var self = this;
+			if (self.options.addClassBeforeAnimation) {
+				self.holder.addClass(self.options.activeClass);
+			}
+			self.slider.removeClass(slideHiddenClass);
+			$(document).on('click touchstart', self.outsideClickHandler);
+
+			self.makeCallback('animStart', true);
+			toggleEffects[self.options.effect].show({
+				box: self.slider,
+				speed: self.options.animSpeed,
+				complete: function() {
+					if (!self.options.addClassBeforeAnimation) {
+						self.holder.addClass(self.options.activeClass);
+					}
+					self.makeCallback('animEnd', true);
+				}
+			});
+		},
+		hideSlide: function() {
+			var self = this;
+			if (self.options.addClassBeforeAnimation) {
+				self.holder.removeClass(self.options.activeClass);
+			}
+			$(document).off('click touchstart', self.outsideClickHandler);
+
+			self.makeCallback('animStart', false);
+			toggleEffects[self.options.effect].hide({
+				box: self.slider,
+				speed: self.options.animSpeed,
+				complete: function() {
+					if (!self.options.addClassBeforeAnimation) {
+						self.holder.removeClass(self.options.activeClass);
+					}
+					self.slider.addClass(slideHiddenClass);
+					self.makeCallback('animEnd', false);
+				}
+			});
+		},
+		destroy: function() {
+			this.slider.removeClass(slideHiddenClass).css({
+				display: ''
+			});
+			this.opener.off(this.options.event, this.eventHandler);
+			this.holder.removeClass(this.options.activeClass).removeData('OpenClose');
+			$(document).off('click touchstart', this.outsideClickHandler);
+		},
+		makeCallback: function(name) {
+			if (typeof this.options[name] === 'function') {
+				var args = Array.prototype.slice.call(arguments);
+				args.shift();
+				this.options[name].apply(this, args);
+			}
+		}
+	};
+
+	// add stylesheet for slide on DOMReady
+	var slideHiddenClass = 'js-slide-hidden';
+	(function() {
+		var tabStyleSheet = $('<style type="text/css">')[0];
+		var tabStyleRule = '.' + slideHiddenClass;
+		tabStyleRule += '{position:absolute !important;left:-9999px !important;top:-9999px !important;display:block !important}';
+		if (tabStyleSheet.styleSheet) {
+			tabStyleSheet.styleSheet.cssText = tabStyleRule;
+		} else {
+			tabStyleSheet.appendChild(document.createTextNode(tabStyleRule));
+		}
+		$('head').append(tabStyleSheet);
+	}());
+
+	// animation effects
+	var toggleEffects = {
+		slide: {
+			show: function(o) {
+				o.box.stop(true).hide().slideDown(o.speed, o.complete);
+			},
+			hide: function(o) {
+				o.box.stop(true).slideUp(o.speed, o.complete);
+			}
+		},
+		fade: {
+			show: function(o) {
+				o.box.stop(true).hide().fadeIn(o.speed, o.complete);
+			},
+			hide: function(o) {
+				o.box.stop(true).fadeOut(o.speed, o.complete);
+			}
+		},
+		none: {
+			show: function(o) {
+				o.box.hide().show(0, o.complete);
+			},
+			hide: function(o) {
+				o.box.hide(0, o.complete);
+			}
+		}
+	};
+
+	// jQuery plugin interface
+	$.fn.openClose = function(opt) {
+		var args = Array.prototype.slice.call(arguments);
+		var method = args[0];
+
+		return this.each(function() {
+			var $holder = jQuery(this);
+			var instance = $holder.data('OpenClose');
+
+			if (typeof opt === 'object' || typeof opt === 'undefined') {
+				$holder.data('OpenClose', new OpenClose($.extend({
+					holder: this
+				}, opt)));
+			} else if (typeof method === 'string' && instance) {
+				if (typeof instance[method] === 'function') {
+					args.shift();
+					instance[method].apply(instance, args);
+				}
+			}
+		});
+	};
 }(jQuery));
