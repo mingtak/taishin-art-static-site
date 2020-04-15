efc6ed08e702f14561e12a21184aefca4715efec
Andy Fang <andy@mingtak.com.tw>
2020-04-15 18:34:45 +0800

    update slider autoplay at app.js

--- src/js/app.js
+++ src/js/app.js
@@ -94,6 +94,8 @@ function initAos() {
 
 function initSlickSlider() {
 	jQuery(".slider").slick({
+                autoplay: true,
+                autoplaySpeed: 5000,
 		arrows: false,
 		fade: true,
 		dots: true,
